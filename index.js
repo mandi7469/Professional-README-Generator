32d797e60dd124f3e3ff694e52d927c925cee1a9
mandi7469 <mandi7469@aol.com>
2024-07-19 18:17:07 -0400

    added questions for user input, inported fs and generated markdown

--- index.js
+++ index.js
@@ -1,10 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require ("./utils/generateMarkdown")
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'Project Title',
+      },
+      {
+        type: 'input',
+        message: 'Description?',
+        name: 'Description',
+      },
+      {
+        type: 'input',
+        message: 'Table of Contents?',
+        name: 'Table of Contents',
+      },
+      {
+        type: 'input',
+        message: 'Installation?',
+        name: 'Installation',
+      },
+      {
+        type: 'input',
+        message: 'Usage?',
+        name: 'Usage',
+      },
+      {
+        type: 'input',
+        message: 'Credits?',
+        name: 'Credits',
+      },
+      {
+        type: 'input',
+        message: 'License?',
+        name: 'License',
+      },
+      {
+        type: 'input',
+        message: 'Contributing?',
+        name: 'Contributing',
+      },
+      {
+        type: 'input',
+        message: 'Tests?',
+        name: 'Tests',
+      },
+      {
+        type: 'input',
+        message: 'Questions?',
+        name: 'Questions',
+      },
+      
+];
+
+inquirer
+  .prompt(questions)
+  .then((response) =>{
+    console.log(response)
+  });
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
