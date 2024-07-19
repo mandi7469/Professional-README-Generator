f2edfd1a17a1d39f4afde462264ffc779dacce0f
mandi7469 <mandi7469@aol.com>
2024-07-19 19:53:49 -0400

    deleted a line of code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const generateMarkdown = require ("./utils/generateMarkdown");
-const { log } = require("console");
 
 
 // TODO: Create an array of questions for user input
