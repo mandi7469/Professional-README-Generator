fb96e5c0ee615a42323d229c5bed456a653e13a5
mandi7469 <mandi7469@aol.com>
2024-07-22 18:40:26 -0400

    completed generated markdown template and working on license functions

--- index.js
+++ index.js
@@ -13,42 +13,39 @@ const questions = [
       },
       {
         type: 'input',
-        message: 'Description?',
+        message: 'Provide a short description explaining the what, why, and how of your project',
         name: 'description',
       },
+      // {
+      //   type: 'checkbox',
+      //   message: 'Select Table of Contents',
+      //   name: 'tableOfContents',
+      //   choices: ['Installation', 'Usage', 'Credits', 'License'],
+      // },
       {
         type: 'input',
-        message: 'Table of Contents?',
-        name: 'tableOfContents',
-      },
-      {
-        type: 'input',
-        message: 'Installation?',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
         name: 'installation',
       },
       {
         type: 'input',
-        message: 'Usage?',
+        message: 'Provide instructions and examples for use',
         name: 'usage',
       },
       {
         type: 'input',
-        message: 'Credits?',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
         name: 'credits',
       },
       {
-        type: 'input',
-        message: 'License?',
+        type: 'list',
+        message: 'Choose a license',
         name: 'license',
+        choices: ['None', 'Apache License 2.0,', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense' ]
       },
       {
         type: 'input',
-        message: 'Contributing?',
-        name: 'contributing',
-      },
-      {
-        type: 'input',
-        message: 'Tests?',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
         name: 'tests',
       },
       {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub License](https://img.shields.io/badge/license-${license.replace(' ', '_')}-blue.svg)`; 
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +23,69 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data);
-  return `# ${data.projectTitle}`;
+  return `
+  # ${data.projectTitle}
+  
+  ## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+Include screenshots as needed.
+
+To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    md
+    ![alt text](assets/images/screenshot.png)
+    
+
+## Credits
+
+${data.credits}
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+`;
 }
 
 module.exports = generateMarkdown;
